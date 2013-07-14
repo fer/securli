2bff26a6d946e2ae569b1bbaf383f8a9f92dbfb1
fer <fer@ferqwerty.com>
2013-07-15 00:51:56 +0200

    implemented basic filestorage sharding - run npm install - TODO: fix message's tests

--- securli/app.js
+++ securli/app.js
@@ -11,115 +11,135 @@ var COOKIE_SECRET = '83be60e3e0435fc2e07bc79fe84b4567',
     Mail = require('model/email'),
     path = require('path');
 
-var app = express();
+// uploadfs init options
+var STORAGE_DIR = __dirname + '/storage',
+    BASE_URL = "localhost:3000",
+    uploadsLocalUrl = '/storage',
+    uploadfs_options = {
+        backend: 'local',
+        uploadsPath: STORAGE_DIR,
+        uploadsUrl: 'http://' + BASE_URL + uploadsLocalUrl,
+        tempPath: __dirname + '/temp'
+    };
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'hjs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser(COOKIE_SECRET));
-app.use(express.session());
-app.use(app.router);
-app.use(require('less-middleware')({
-    src: __dirname + '/public'
-}));
+// uploadfs init
+Message.localFileSystem.init(uploadfs_options, runApp());
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
+// runs express app
+function runApp() {
+    var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+    // all environments
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'hjs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser(COOKIE_SECRET));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(require('less-middleware')({
+        src: __dirname + '/public'
+    }));
 
-app.get('/success/:id', function(req, res) {
-    if( req.session.messageId !== req.params.id ){
-        // you can only view this page if you created this
-        res.redirect( '/error' );
+    app.use(uploadsLocalUrl, express.static(STORAGE_DIR));
+
+    // development only
+    if ('development' == app.get('env')) {
+        app.use(express.errorHandler());
     }
 
-    Message.load( req.params.id, function( err, data ){
-        if( err ){
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.get('/success/:id', function(req, res) {
+        if( req.session.messageId !== req.params.id ){
+            // you can only view this page if you created this
             res.redirect( '/error' );
-            return;
         }
 
-        res.render('success', {
-            title: 'Securli',
-            id: req.params.id,
-            email: data.email
-        });
-    });
-});
+        Message.load( req.params.id, function( err, data ){
+            if( err ){
+                res.redirect( '/error' );
+                return;
+            }
 
-app.get('/delete/:id', function(req, res) {
-
-    Message.delete( req.params.id, function( err ){
-        if( err ){
-            res.redirect( '/error' );
-            return;
-        }
-
-        res.redirect( 303, '/' );
-    });
-});
-
-app.get('/view/:id', function(req, res) {
-    Message.load( req.params.id, function( err, data ){
-        if( err ){
-            res.redirect( '/error' );
-            return;
-        }
-
-        res.render('view', {
-            title: 'Securli',
-            id: req.params.id,
-            message: data.message
-        });
-    });
-});
-
-app.get('/error', function(req, res) {
-    res.status(404);
-    res.render('error', {
-        title: 'Securli - an error occured'
-    });
-});
-
-app.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Securli'
-    });
-});
-
-app.post('/', function(req, res) {
-    if (req.body.message && req.body.email) {
-        util.log('creating new message for: ' + req.body.email);
-
-        Message.create(req.body, function(err, data) {
-            var email = new Mail( data, req.body.email, req );
-
-            email.send(function(err){
-                if( err ) {
-                    util.error( err, 'unknow error');
-                    res.redirect('/error');
-                    return;
-                }
-
-                req.session.messageId = data.id;
-                res.redirect( 303, '/success/' + data.id);
+            res.render('success', {
+                title: 'Securli',
+                id: req.params.id,
+                email: data.email
             });
         });
+    });
 
-        return;
-    }
+    app.get('/delete/:id', function(req, res) {
 
-    res.redirect('/?error=' + (req.body.message ? 'email' : 'message'));
-});
+        Message.delete( req.params.id, function( err ){
+            if( err ){
+                res.redirect( '/error' );
+                return;
+            }
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+            res.redirect( 303, '/' );
+        });
+    });
+
+    app.get('/view/:id', function(req, res) {
+        Message.load( req.params.id, function( err, data ){
+            if( err ){
+                res.redirect( '/error' );
+                return;
+            }
+
+            res.render('view', {
+                title: 'Securli',
+                id: req.params.id,
+                message: data.message
+            });
+        });
+    });
+
+    app.get('/error', function(req, res) {
+        res.status(404);
+        res.render('error', {
+            title: 'Securli - an error occured'
+        });
+    });
+
+    app.get('/', function(req, res) {
+        res.render('index', {
+            title: 'Securli'
+        });
+    });
+
+    app.post('/', function(req, res) {
+        if (req.body.message && req.body.email) {
+            util.log('creating new message for: ' + req.body.email);
+
+            Message.create(req.body, function(err, data) {
+                var email = new Mail( data, req.body.email, req );
+
+                email.send(function(err){
+                    if( err ) {
+                        util.error( err, 'unknow error');
+                        res.redirect('/error');
+                        return;
+                    }
+
+                    req.session.messageId = data.id;
+                    res.redirect( 303, '/success/' + data.id);
+                });
+            });
+
+            return;
+        }
+
+        res.redirect('/?error=' + (req.body.message ? 'email' : 'message'));
+    });
+
+    http.createServer(app).listen(app.get('port'), function() {
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+}
+
--- securli/lib/model/message.js
+++ securli/lib/model/message.js
@@ -3,54 +3,99 @@
  */
 
 var Message,
-    STORAGE_DIR,
     crypto = require('crypto'),
     path = require('path'),
     fs = require('fs'),
-    util = require('util');
+    util = require('util'),
+    uploadfs = require('uploadfs')(),
+    http = require("http");
 
-STORAGE_DIR = path.resolve(__dirname + '/../../storage');
+var MD5_PASSWORD = "47f^7)9f&$#Ee3698!@#";
+var STORAGE_DIR = path.resolve(__dirname + '/../../storage');
+
 
 function _md5_digest(data) {
-    var hash = crypto.createHash('md5');
-
+    var hash = crypto.createHash('md5', MD5_PASSWORD);
     hash.update(data);
-    hash.update(Date.now() + '');
-
     return hash.digest('hex');
 }
 
+
 function _get_path(digest) {
-    return util.format('%s/%s.json', STORAGE_DIR, digest);
+    // create md5 hash from the first char in 'digest'
+    // to be used as a folder for similar messages
+    return util.format('%s/%s/%s.json', STORAGE_DIR, _md5_digest(digest[0]), digest);
 }
+
 
 Message = {
     create: function(data, fnReady) {
         var json = JSON.stringify(data),
             digest = _md5_digest(json);
 
-        fs.writeFile(_get_path(digest), json, function(err) {
+        var outputFilename = digest + '.json',
+            tempOutputFilepath = '/tmp/' + outputFilename;
+
+        var outputFolderName = _md5_digest(digest[0]),
+            outputFilepath = "/" + outputFolderName + "/" + outputFilename;
+
+        fs.writeFile(tempOutputFilepath, json, function(err) {
             if (err) {
                 util.error(err);
+            } else {
+                uploadfs.copyIn(tempOutputFilepath, outputFilepath,
+                    function(e, info) {
+                        if (e) {
+                            util.error(e);
+                        } else {
+                            util.log('saved file: ' + outputFilepath);
+                        }
+                        fnReady(err, {
+                            id: digest
+                        });
+                    }
+                );
             }
-            fnReady(err, {
-                id: digest
-            });
         });
     },
 
     load: function(id, fnReady) {
-        fs.readFile(_get_path(id), 'utf8', function(err, json) {
-            var data = err ? null : JSON.parse(json);
+        var msg_url = uploadfs.getUrl() +
+            "/" + _md5_digest(id[0]) +
+            "/" + id + ".json";
+        var err, data;
 
-            fnReady(err, data);
+        // do the GET request
+        var reqGet = http.get(msg_url, function(res) {
+
+            var body = '';
+
+            res.on('data', function(chunk) {
+                body += chunk;
+            });
+
+            res.on('end', function() {
+                if (res.statusCode == 200) {
+                    data = err ? null : JSON.parse(body);
+                    util.log("Got response: ", JSON.parse(body));
+                } else {
+                    err = {
+                        "message": "couldn't find message with id: " + id
+                    };
+                    util.error(err);
+                }
+                fnReady(err, data);
+            });
+        }).on('error', function(e) {
+            fnReady(e, {});
         });
     },
 
     delete: function(id, fnReady) {
-        fs.unlink(_get_path(id), fnReady);
+        uploadfs.remove("/" + _md5_digest(id[0]) + "/" + id + ".json", fnReady);
     }
 };
 
 
 module.exports = Message;
+module.exports.localFileSystem = uploadfs;
